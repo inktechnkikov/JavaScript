feac5e2c0e94962b031a5f81a7cf0245034dcce2
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-08-08 17:07:04 +0300

    update

--- Arrays/ArrayOfIntegers.js
+++ Arrays/ArrayOfIntegers.js
@@ -6,7 +6,27 @@ function nums() {
 }
 function stringOfDays() {
     let weekDays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-    console.log(weekDays);
+    for(let index = 0;index < weekDays.length;index++){
+        console.log(weekDays[index]);
+    }
+    let reversed = new Array(weekDays);
+    reversed.reverse();
+    console.log(reversed);
+}
+function stringOfTowns() {
+    let capitals = ["Sofia","Washington","Berlin"];
+    capitals.forEach(function (capital) {
+        console.log(capital)
+    });
+   capitals.forEach(function (capital,number) {
+       console.log(number + " - > " + " " +  capital);
+   })
+}
+function dates() {
+    let dataArr = [new Date()];
+    console.log(dataArr);
 }
 nums();
 stringOfDays();
+dates();
+stringOfTowns();

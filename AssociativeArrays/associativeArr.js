20a77853db5d3c25e6fa51ad1c0cee19ed3f0163
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-08-08 18:52:20 +0300

    associative arrays

---
+++ AssociativeArrays/associativeArr.js
@@ -0,0 +1,12 @@
+function test() {
+    let prices = {
+        "orange":2.30,
+        "apple":1.20,
+        "cucumber":2.0,
+        "tomato":1.50,
+        "watermelon":0.50
+    };
+    console.log(prices);
+    console.log("watermelon price - " + prices.watermelon);
+}
+test();

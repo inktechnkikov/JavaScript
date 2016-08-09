bcc84e25e70dcebe20fe691e62a49365dc8dfb34
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-08-09 13:52:59 +0300

    objects

---
+++ JS-Object/scores.js
@@ -0,0 +1,8 @@
+let marks = [
+    {subject:'Java',scores : 6.00},
+    {subject:'HTML',scores:5.90},
+    {subject:'JavaScript',scores:6.00},
+    {subject:'PHP',scores:6.00}
+];
+let student = {fullName:"Cvetko Cvetanov",studentMarks:marks};
+student.studentMarks[0].scores = 5.00;
